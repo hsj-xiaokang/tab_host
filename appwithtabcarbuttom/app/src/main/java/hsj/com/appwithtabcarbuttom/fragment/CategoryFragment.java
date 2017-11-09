9defd00dc33dcdca40f04f6ae9b8c8c957f49a6b
2356899074@qq.com <2356899074@qq.com>
2017-11-09 17:08:29 +0800

    源码 来自github原作者https://github.com/wuyinlei/FragmentTabHost.git

---
+++ appwithtabcarbuttom/app/src/main/java/hsj/com/appwithtabcarbuttom/fragment/CategoryFragment.java
@@ -0,0 +1,22 @@
+package hsj.com.appwithtabcarbuttom.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hsj.com.appwithtabcarbuttom.R;
+
+/**
+ * Created by ruolan on 2015/11/29.
+ */
+public class CategoryFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container,false);
+        return view;
+    }
+}
